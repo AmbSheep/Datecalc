32c2dc88aa3d771738154965c55ef1d14b529bae
AmbSheep <creamghost@mail.ru>
2020-07-08 11:24:48 +0300

    fixed formats

--- datecalc.py
+++ datecalc.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import sys
 import datetime
+from dateutil.parser import parse
 
 
 def datecalc(start_date, days):
-    date_1 = datetime.datetime.strptime(start_date, "%d-%m-%Y")
+    """
+    function to add days to specified date
+    :param start_date: date in any format
+    :param days: days to add
+    :return: returns date after days addition
+    """
+    date_1 = parse(start_date)
     end_date = date_1 + datetime.timedelta(days=days)
-    return end_date.strftime("%d-%m-%Y")
+    return end_date
 
 
 if __name__ == "__main__":
-    start_date = sys.argv[1]
-    days = int(sys.argv[2])
-    print(datecalc(start_date, days))
+    start_date = input()
+    days = int(input())
+    print(datecalc(start_date, days).date())
